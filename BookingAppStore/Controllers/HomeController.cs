16a1edb4952c6699e4572863d693d4b4595c5dd6
Калашников Андрей <nf2g.2000@gmail.com>
2020-01-31 01:41:28 +0700

    Добавьте файлы проекта.

---
+++ BookingAppStore/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using BookingAppStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingAppStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+        public ActionResult Index()
+        {
+            var books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.Bookid = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + purchase.Person + ", за покупку!";
+        }
+    }
+}
